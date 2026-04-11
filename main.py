e2b468220828fa3d64992339a405501c8a8ddfa3
J4n23 <vondrsen@gmail.com>
2026-04-11 22:05:30 +0200

    Rework - actors/{film_title} into actors/{first_name}/{last_name} :

--- main.py
+++ main.py
@@ -22,29 +22,38 @@ def list_films():
 
 @app.get("/films/{film_id}")
 def view_film(film_id: int = Path (description="Details of 1 film")):
-    resultDIctionary = dict()
+    #resultDIctionary = dict()
     mycursor.execute("SELECT film_id, title, description, release_year FROM film WHERE film_id = %s;", (film_id,))
-    querry1 = mycursor.fetchone()
+    query1 = mycursor.fetchone()
     mycursor.execute("SELECT ac.actor_id, CONCAT ( ac.first_name,' ', ac.last_name) AS actor_name " 
                     "from film flm "
                     "join film_actor fac on flm.film_id = fac.film_id "
                     "join actor ac on fac.actor_id = ac.actor_id " 
                     "where flm.film_id = %s;", (film_id,))
-    querry2 = mycursor.fetchall()
-    resultDIctionary = querry1
-    resultDIctionary["actors"] = querry2
-    return resultDIctionary
+    query2 = mycursor.fetchall()
+    result_dictionary = query1
+    result_dictionary["actors"] = query2
+    return result_dictionary
 
 
-@app.get("/actors/{film_title}")
-def view_actors(film_title: str):
-    mycursor.execute("SELECT flm.film_id, flm.title, flm.description, flm.release_year, ac.first_name, ac.last_name " 
-                    "from film flm "
-                    "join film_actor fac on flm.film_id = fac.film_id "
-                    "join actor ac on fac.actor_id = ac.actor_id " 
-                    "where flm.film_id = %s;", (film_id,))
-    result = mycursor.fetchall()
-    return result
+@app.get("/actors/{first_name}/{last_name}")
+def view_actors(first_name: str, last_name: str):
+    result_dictionary = dict()
+    mycursor.execute("SELECT flm.film_id, flm.title "
+                    "from actor ac "
+                    "join film_actor fac on ac.actor_id = fac.actor_id "
+                    "join film flm on fac.film_id = flm.film_id "
+                    "where ac.first_name = %s and ac.last_name = %s; ", (first_name, last_name))
+    film_query = mycursor.fetchall()
+    mycursor.execute("SELECT ac.actor_id, CONCAT ( ac.first_name,' ', ac.last_name) AS actor_name "
+                     "from actor ac "
+                     "where ac.first_name = %s and ac.last_name = %s; ", (first_name, last_name))
+    actor_query = mycursor.fetchall()
+    result_dictionary["films"] = film_query
+    result_dictionary["actor"] = actor_query
+    return result_dictionary
+## WHAT IF THERE WILL BE TWO IDENTICALLY NAMED ACTORS??
+
 
 ## DEMO OBJECT WITH NO PURPOSE
 @app.get("/items/{item_id}")
