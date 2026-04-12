8ded87718c2753b33ba5f72d74c5d16fe4aeb48f
J4n23 <vondrsen@gmail.com>
2026-04-12 23:09:12 +0200

    Covering 404 errors

--- main.py
+++ main.py
@@ -22,49 +22,61 @@ def list_films():
 
 @app.get("/films/{film_id}")
 def view_film(film_id: int = Path (description="Details of 1 film")):
-    #resultDIctionary = dict()
     mycursor.execute("SELECT film_id, title, description, release_year FROM film WHERE film_id = %s;", (film_id,))
     query1 = mycursor.fetchone()
-    mycursor.execute("SELECT ac.actor_id, CONCAT ( ac.first_name,' ', ac.last_name) AS actor_name " 
-                    "from film flm "
-                    "join film_actor fac on flm.film_id = fac.film_id "
-                    "join actor ac on fac.actor_id = ac.actor_id " 
-                    "where flm.film_id = %s;", (film_id,))
-    query2 = mycursor.fetchall()
-    result_dictionary = query1
-    result_dictionary["actors"] = query2
-    return result_dictionary
+    if query1:
+        mycursor.execute("SELECT ac.actor_id, CONCAT ( ac.first_name,' ', ac.last_name) AS actor_name " 
+                        "from film flm "
+                        "join film_actor fac on flm.film_id = fac.film_id "
+                        "join actor ac on fac.actor_id = ac.actor_id " 
+                        "where flm.film_id = %s;", (film_id,))
+        query2 = mycursor.fetchall()
+        print(query2)
+        result_dictionary = query1
+        result_dictionary["actors"] = query2
+        return result_dictionary
+    else:
+        raise HTTPException(status_code=404, detail="Film ID Not Found!")
 
 
 @app.get("/actors/{first_name}/{last_name}")
 def view_actors(first_name: str, last_name: str):
     result_dictionary = dict()
-    mycursor.execute("SELECT flm.film_id, flm.title "
-                    "from actor ac "
-                    "join film_actor fac on ac.actor_id = fac.actor_id "
-                    "join film flm on fac.film_id = flm.film_id "
-                    "where ac.first_name = %s and ac.last_name = %s; ", (first_name, last_name))
-    film_query = mycursor.fetchall()
     mycursor.execute("SELECT ac.actor_id, CONCAT ( ac.first_name,' ', ac.last_name) AS actor_name "
                      "from actor ac "
                      "where ac.first_name = %s and ac.last_name = %s; ", (first_name, last_name))
     actor_query = mycursor.fetchall()
-    result_dictionary["films"] = film_query
-    result_dictionary["actor"] = actor_query
-    return result_dictionary
+    if actor_query:
+        mycursor.execute("SELECT flm.film_id, flm.title "
+                        "from actor ac "
+                        "join film_actor fac on ac.actor_id = fac.actor_id "
+                        "join film flm on fac.film_id = flm.film_id "
+                        "where ac.first_name = %s and ac.last_name = %s; ", (first_name, last_name))
+        film_query = mycursor.fetchall()
+        result_dictionary["films"] = film_query
+        result_dictionary["actor"] = actor_query
+        return result_dictionary
+    else:
+        raise HTTPException(status_code=404, detail="No Such Actor In Database!")
+
 
 @app.get("/inventory/{film_id}")
 def view_availability(film_id: int):
-    mycursor.execute("SELECT COUNT(result.inventory_id) AS AVAILABLE "
-                    "FROM ( "
-                        "SELECT DISTINCT rnt.inventory_id, inv.film_id "
-                        "FROM inventory inv "
-                        "JOIN rental rnt ON inv.inventory_id = rnt.inventory_id " 
-                        "WHERE inv.film_id = %s "
-                        "AND rnt.inventory_id NOT IN (SELECT inventory_id from rental where return_date is NULL) "
-	                ") AS result; ", (film_id, ))
-    availability = mycursor.fetchone()
-    return availability
+    mycursor.execute("SELECT inventory_id FROM inventory WHERE film_id = %s;", (film_id,))
+    alive = mycursor.fetchone()
+    if alive:
+        mycursor.execute("SELECT COUNT(result.inventory_id) AS AVAILABLE "
+                        "FROM ( "
+                            "SELECT DISTINCT rnt.inventory_id, inv.film_id "
+                            "FROM inventory inv "
+                            "JOIN rental rnt ON inv.inventory_id = rnt.inventory_id " 
+                            "WHERE inv.film_id = %s "
+                            "AND rnt.inventory_id NOT IN (SELECT inventory_id from rental where return_date is NULL) "
+                        ") AS result; ", (film_id, ))
+        availability = mycursor.fetchone()
+        return availability
+    else:
+        raise HTTPException(status_code=404, detail="No Such Movie In Inventory!")
 
 
 ## DEMO OBJECT WITH NO PURPOSE
